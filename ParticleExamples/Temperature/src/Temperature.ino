824a644e93bd8bb0763337fee6d4b5e39599e00c
Simon Kuhnle <simon.kuhnle@methodpark.de>
2017-04-26 11:17:09 +0200

    Add short text for serial and publishing values

--- ParticleExamples/Temperature/src/Temperature.ino
+++ ParticleExamples/Temperature/src/Temperature.ino
@@ -4,6 +4,11 @@
 // Connect SDA to I2C data pin  (D0 on Photon/Electron)
 //
 // Use Putty or GNU/screen to see the output of the serial line
+// screen /dev/ttyACM0 9600
+// or http://blog.jongallant.com/2015/08/particle-photon-setup-windows-putty/
+//
+// You can see the temperature posted in the particle.io Console under "Logs"
+// and you can create a Webhook that posts this value to whatever service you want
 
 #include "Adafruit_Si7021.h"   // Use for Build IDE
 // #include "Adafruit_Si7021.h"               // Use for local build
@@ -18,7 +23,9 @@ void setup() {
 }
 
 void loop() {
+  float temp = sensor.readTemperature();
   Serial.print("Humidity: "); Serial.println(sensor.readHumidity(), 2);
-  Serial.print("Temperature: "); Serial.println(sensor.readTemperature(), 2);
+  Serial.print("Temperature: "); Serial.println(temp, 2);
+  Particle.publish("temp", String(temp));
   delay(10000);
 }
